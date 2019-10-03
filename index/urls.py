e05f44b9cdf4a18676f85e898de6694ff7a4c5c1
ILya <ILya@Mac.local>
2019-10-03 15:17:33 +0300

    1

--- index/urls.py
+++ index/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('stock/', views.stock, name='stock'),
     path('bank/', views.bank, name='bank'),
     path('book_list/', ListView.as_view(queryset=Book.objects.all().order_by("author")), name='book_list.html'),
-    path('message_list/', ListView.as_view(queryset=Message.objects.all().order_by("-date")), name='message_list.html'),
+    path('message_list/', views.message_list, name='message_list.html'),
 ]
+
+# ListView.as_view(queryset=Message.objects.all().order_by("-date"))

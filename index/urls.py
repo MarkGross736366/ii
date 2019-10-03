daeebfdd9444ebae0472b1b788d421cabf029838
ILya <ILya@Mac.local>
2019-10-03 15:26:08 +0300

    1

--- index/urls.py
+++ index/urls.py
@@ -4,13 +4,11 @@ from . import views
 from index.models import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, ListView.as_view(queryset=Message.objects.all().order_by("-date")), name='index'),
     path('chart/', views.chart, name='chart'),
     path('ipo/', views.ipo, name='ipo'),
     path('stock/', views.stock, name='stock'),
     path('bank/', views.bank, name='bank'),
     path('book_list/', ListView.as_view(queryset=Book.objects.all().order_by("author")), name='book_list.html'),
-    path('message_list/', views.message_list, name='message_list.html'),
+    path('message_list/', ListView.as_view(queryset=Message.objects.all().order_by("-date")), name='message_list.html'),
 ]
-
-# ListView.as_view(queryset=Message.objects.all().order_by("-date"))
